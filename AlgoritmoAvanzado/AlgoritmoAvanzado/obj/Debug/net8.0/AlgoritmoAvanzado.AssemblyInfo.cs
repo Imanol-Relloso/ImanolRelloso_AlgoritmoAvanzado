d3d4d8ede7269c6cb9b6c7755a9b2b1e5a9088e4
Imanol Relloso <imanol_relloso@euneiz.com>
2024-12-17 09:44:53 +0100

    .gitignore added

--- AlgoritmoAvanzado/AlgoritmoAvanzado/obj/Debug/net8.0/AlgoritmoAvanzado.AssemblyInfo.cs
+++ AlgoritmoAvanzado/AlgoritmoAvanzado/obj/Debug/net8.0/AlgoritmoAvanzado.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgoritmoAvanzado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce4435ca305de3c821442267e674be03a48605d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgoritmoAvanzado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgoritmoAvanzado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
