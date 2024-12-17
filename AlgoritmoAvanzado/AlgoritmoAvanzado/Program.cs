9d02082c676642a91686db47a2a6360ea037e027
Imanol Relloso <imanol_relloso@euneiz.com>
2024-12-17 08:52:22 +0100

    Ask user the numbers

--- AlgoritmoAvanzado/AlgoritmoAvanzado/Program.cs
+++ AlgoritmoAvanzado/AlgoritmoAvanzado/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AlgoritmoAvanzado.Classes;
+
+List<int> listanumeros = new List<int>();
+
+ConsoleKey continuar;
+//Preguntar al usuario los números
+do
+{
+    Console.Clear();
+    Console.WriteLine("\nIntroduzca un número:");
+    string respuesta = Console.ReadLine();
+    int number = int.Parse(respuesta);
+
+    //Añadir números a la lista
+    listanumeros.Add(number);
+
+    //Preguntar si quiere seguir añadiendo números
+    Console.WriteLine("\n¿Quiere introducir más números? s/n");
+    continuar = Console.ReadKey().Key;
+} while (continuar == ConsoleKey.S);
+
+Console.Clear();
+Console.WriteLine(listanumeros);
